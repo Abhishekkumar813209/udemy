29d2252b9b96e6175d224161ef1ca77f563a8ca3
Bibash Roy <bibhash@gmail.com>
2023-07-10 16:47:08 +0530

    added getRazorpaykey  (but still payment verification route is not complete)

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,11 +1,18 @@
 import express from "express";
 import { isAuthenticated } from "../middlewares/auth.js";
-import {buySubscription} from "../controllers/paymentController.js";
+import {buySubscription, getRazorPayKey} from "../controllers/paymentController.js";
 
 const router = express.Router();
 
 
 //Buy Subscription
 router.route("/subscribe").get(isAuthenticated,buySubscription)
+
+//verify payment and save reference in database
+router.route("/paymentverification").post(isAuthenticated,buySubscription);
+
+//Get Razorpay Key
+router.route("/razorpaykey").get(getRazorPayKey);
+
 
 export default router;
