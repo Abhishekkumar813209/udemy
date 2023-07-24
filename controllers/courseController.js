89f48a3ec27c5b04fafe874b366cf76e701b81b8
Bibash Roy <bibhash@gmail.com>
2023-07-24 19:47:16 +0530

    removedregex from get all courses controller to get all courses

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -10,16 +10,7 @@ export const getAllCourse = catchAsyncError(async(req,res,next)=>{
     const keyword = req.query.keyword || " "
     const category = req.query.category || " "
 
-    const courses = await Course.find({
-        title:{
-            $regex:keyword,
-            $options:"i"
-        },
-        category:{
-            $regex:category,
-            $options:"i"
-        }
-    }).select("-lectures");
+    const courses = await Course.find().select("-lectures");
     res.status(200).json({
         success:true,
         courses,
